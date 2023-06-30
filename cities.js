25c5493d0406491adc4c70c1b92bfe6bb00e01f5
conveypride <conveypride@gmail.com>
2023-06-30 15:58:16 +0100

    weather app

---
+++ cities.js
@@ -0,0 +1,63 @@
+export default [
+  {
+    "country": "GH",
+    "name": "Accra",
+    "lat": "5.6037",
+    "lng": "-0.1870"
+},
+{
+    "country": "GH",
+    "name": "Kumasi",
+    "lat": "6.6885",
+    "lng": "-1.6244"
+},
+{
+    "country": "GH",
+    "name": "Tamale",
+    "lat": "9.4008",
+    "lng": "-0.8393"
+},
+{
+    "country": "GH",
+    "name": "Sekondi-Takoradi",
+    "lat": "4.9437",
+    "lng": "-1.7590"
+},
+{
+    "country": "GH",
+    "name": "Sunyani",
+    "lat": "7.3314",
+    "lng": "-2.3274"
+},
+{
+    "country": "GH",
+    "name": "Cape Coast",
+    "lat": "5.1033",
+    "lng": "-1.2466"
+},
+{
+    "country": "GH",
+    "name": "Koforidua",
+    "lat": "6.0902",
+    "lng": "-0.2590"
+},
+{
+    "country": "GH",
+    "name": "Wa",
+    "lat": "10.0600",
+    "lng": "-2.5070"
+},
+{
+    "country": "GH",
+    "name": "Ho",
+    "lat": "6.6128",
+    "lng": "0.4680"
+},
+{
+    "country": "GH",
+    "name": "Techiman",
+    "lat": "7.5858",
+    "lng": "-1.9417"
+}
+
+];
